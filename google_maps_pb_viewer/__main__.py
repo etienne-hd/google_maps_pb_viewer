e810c4db32de78c1dfd2262878ca9a60053fa723
etienne-hd <hode.etienne@gmail.com>
2026-02-26 17:43:34 +0100

    style: rename pb variable by part/parts

--- google_maps_pb_viewer/__main__.py
+++ google_maps_pb_viewer/__main__.py
@@ -1,22 +1,22 @@
 import argparse
 
-def get_parsed_pb(pb: str) -> tuple[str, str, str]:
-	for i, c in enumerate(pb):
+def get_parsed_pb(part: str) -> tuple[str, str, str]:
+	for i, c in enumerate(part):
 		if c.isalpha():
-			return pb[:i], pb[i], pb[i + 1:]
+			return part[:i], part[i], part[i + 1:]
 
-def print_pb(pb: list[str], indent: int = 4, depth: int = 0, n: int = -1) -> int:
+def print_pb(parts: list[str], indent: int = 4, depth: int = 0, n: int = -1) -> int:
 	indent_length = indent * depth
 
 	i = 0
-	while i < len(pb) and (n == -1 or i < n):
-		current_pb = pb[i]
+	while i < len(parts) and (n == -1 or i < n):
+		part = parts[i]
 		i += 1
-		field, pb_type, value = get_parsed_pb(current_pb)
+		field, pb_type, value = get_parsed_pb(part)
 		print(indent_length * " ", f"{field}: ", sep="", end="")
 		if pb_type == "m":
 			print("{")
-			i += print_pb(pb=pb[i:], indent=indent, depth=depth + 1, n=int(value))
+			i += print_pb(parts=parts[i:], indent=indent, depth=depth + 1, n=int(value))
 			print(indent_length * " ", "}", sep="")
 		elif pb_type == "b":
 			print("true" if value == "1" else "false")
@@ -25,25 +25,24 @@ def print_pb(pb: list[str], indent: int = 4, depth: int = 0, n: int = -1) -> int
 	return i
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        description="Decode and pretty-print a Google Maps encoded protobuf string."
-    )
+	parser = argparse.ArgumentParser(
+		description="Decode and pretty-print a Google Maps encoded protobuf string."
+	)
 
-    parser.add_argument(
-        "pb",
-        type=str,
-        help="Encoded Google Maps protobuf string (starting with '!')."
-    )
+	parser.add_argument(
+		"pb",
+		type=str,
+		help="Encoded Google Maps protobuf string (starting with '!')."
+	)
 
-    parser.add_argument(
-        "-i",
-        "--indent",
-        type=int,
-        default=4,
-        help="Number of spaces used for output indentation (default: 4)."
-    )
+	parser.add_argument(
+		"-i",
+		"--indent",
+		type=int,
+		default=4,
+		help="Number of spaces used for output indentation (default: 4)."
+	)
 
-    args = parser.parse_args()
-
-    pb = args.pb.split("!")[1:]
-    print_pb(pb=pb, indent=args.indent)
+	args = parser.parse_args()
+	parts = args.pb.split("!")[1:]
+	print_pb(parts=parts, indent=args.indent)
